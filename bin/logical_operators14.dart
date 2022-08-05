29351178b069a84a6964cd871975ce8a69d1d3f8
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-05 14:00:57 +0500

    14

--- bin/logical_operators14.dart
+++ bin/logical_operators14.dart
@@ -9,14 +9,12 @@ Given a two-digit integer a,  check the following statement "All digits sum is o
 */
 
 bool func(int a) {
-  if (a > 9 && a < 100) {
-    if ((a % 10 + a ~/ 10) % 2 == 1) {
-      return true;
-    }
+  if (a > 9 && a < 100 && (a % 10 + a ~/ 10) % 2 == 1) {
+    return true;
   }
   return false;
 }
 
 void main() {
-  print(func(60));
+  print(func(70));
 }
