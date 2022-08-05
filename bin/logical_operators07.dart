dbd433725c80cadd1947d19d7713b3283a200096
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-05 08:53:06 +0500

    07

--- bin/logical_operators07.dart
+++ bin/logical_operators07.dart
@@ -9,4 +9,13 @@ Given two integers a, b,  check the following statement "At least one  of the nu
         bool: answer
 */
 
-void main() {}
+bool func(int a, int b) {
+  if (a < 0 && b > 0 || a > 0 && b < 0) {
+    return true;
+  }
+  return false;
+}
+
+void main() {
+  print(func(4, 5));
+}
